a7fc2a3da01f2ca492a9689e3aaf38351ad58f7c
Valery Karnilayeu <valerykarnilayeu@gmail.com>
2024-11-04 12:48:48 +0300

    fix files

--- src/CustomArrayList.java
+++ src/CustomArrayList.java
@@ -1,24 +1,31 @@
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 
-public class СustomArrayList<E> {
-    Object[] customArr = new Object[10];
+
+public class CustomArrayList<E> {
+    private Object[] customArr = new Object[10];
+    private int size;
 
     public void add(int index, E element) {
 
         if(customArr[index] != null) {
             Object[] customDestArrayList = new Object[customArr.length + 1];
             if (index < customArr.length && index > 0) {
+
                 System.arraycopy(customArr, 0, customDestArrayList, 0, index);
                 customDestArrayList[index] = element;
                 System.arraycopy(customArr, index, customDestArrayList, index + 1, customArr.length - index);
+
                 customArr = customDestArrayList;
+                size++;
+
             } else {
                 throw new RuntimeException("IndexOutOfBoundsException");
             }
         } else {
             customArr[index] = element;
+            size++;
         }
     }
 
@@ -29,10 +36,12 @@ public class СustomArrayList<E> {
                 '}';
     }
 
+
     public void clear() {
         for(int i = 0; i < customArr.length; i++) {
             customArr[i] = null;
         }
+        size = 0;
     }
 
     public E get(int index) {
@@ -43,58 +52,86 @@ public class СustomArrayList<E> {
     }
 
     public boolean isEmpty() {
-        int count = 0;
-        for(int i = 0; i < customArr.length; i++) {
-            if(customArr[i] != null) {
-                count++;
-            }
+        if(size == 0) {
+            return true;
         }
-        if(count > 0) {
-            return false;
-        }
-        return true;
+        return false;
     }
 
     public boolean remove(int index) {
         Object[] customDestArrayList = new Object[customArr.length - 1];
             if(customArr[index] != null) {
-                System.arraycopy(customArr, 0, customDestArrayList, 0, index);
 
+                System.arraycopy(customArr, 0, customDestArrayList, 0, index);
                 System.arraycopy(customArr, index + 1, customDestArrayList, index, customArr.length - 1 - index);
-                System.out.println(customDestArrayList);
-                System.out.println("true");
+
                 customArr = customDestArrayList;
+                --size;
                 return true;
             }
-
-        System.out.println("false");
         return false;
     }
 
 
     public boolean remove(Object o) {
-        Object[] customDestArrayList = new Object[customArr.length - 1];
         for(int i = 0; i < customArr.length; i++) {
             if(o == null) {
-                System.out.println("false");
                 return false;
             } else {
                 if(o.equals(customArr[i])) {
                     remove(i);
+                    --size;
                     return true;
                 }
             }
         }
-        System.out.println("false");
         return false;
     }
 
     public boolean addAll(Collection<? extends E> c) {
-        Object[] arrObj = c.toArray();
-        if(arrObj.length == 0) {
+        Object[] arrObjects = c.toArray();
+        if (arrObjects.length == 0) {
             return false;
         }
-        Object[] newArr = new Object[customArr.length + arrObj.length];
 
+        Object[] customArr = this.customArr;
+        Object[] interArr = new Object[customArr.length + arrObjects.length];
+
+        System.arraycopy(customArr, 0, interArr, 0, customArr.length);
+        System.arraycopy(arrObjects, 0, interArr, size, arrObjects.length);
+        this.customArr = interArr;
+
+        return true;
+    }
+
+
+    public void sort(Comparator<? super E> c) {
+        quickSort(c, customArr, 0, size - 1);
+    }
+
+    public void quickSort(Comparator<? super E> c, Object[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(c, arr, low, high);
+            quickSort(c, arr, low, pi - 1);
+            quickSort(c, arr, pi + 1, high);
+        }
+    }
+
+    private int partition(Comparator<? super E> c, Object[] arr, int low, int high) {
+        Object pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (c.compare((E)arr[j], (E)pivot) < 0 ) {
+                i++;
+                Object temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        Object temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,34 @@
-import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        СustomArrayList<String> arr = new СustomArrayList<>();
-        arr.add(4,"4");
-        arr.add(5,"5");
-        arr.add(3,"in");
+        CustomArrayList<String> arr = new CustomArrayList<>();
+        List<String> arr1 = List.of("ko", "le", "bi");
 
-        System.out.println(arr);
+        arr.add(0,"an");
+        arr.add(1,"pn");
+        arr.add(2,"in");
+        arr.add(2,"en");
+        arr.add(2,"qn");
+
+
         arr.remove("in");
-//        System.out.println(arr);
-//        arr.clear();
         System.out.println(arr);
 
-//       arr.get(5);
-//        System.out.println(arr.isEmpty());
+        arr.remove(3);
+        System.out.println(arr);
+
+        System.out.println(arr.get(1));
+
+        System.out.println(arr.isEmpty());
+        arr.clear();
+        System.out.println(arr.isEmpty());
+
+        arr.addAll(arr1);
+        System.out.println(arr);
+
+        arr.sort(String::compareTo);
+        System.out.println(arr);
+
     }
 }
